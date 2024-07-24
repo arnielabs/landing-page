c94b7a6542c7a9675e4cf76e4689a8f3969b0cf7
r-arvind <rarvind1999@gmail.com>
2024-07-25 00:22:45 +0530

    Fixed build warnings and issues

--- src/app/(default)/page.js
+++ src/app/(default)/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     
     <main className="w-full relative h-[calc(100vh-4rem)] md:h-[calc(100vh-4rem)] flex items-center justify-center text-center">
         <a className="absolute h-full w-full">
-        <img className="lazyload absolute w-full h-full object-cover pointer-events-none entered loaded" src="/bg3.jpeg"></img>
+        <Image className="lazyload absolute w-full h-full object-cover pointer-events-none entered loaded" src="/bg3.jpeg" alt="Soundframe Image"/>
         </a>
         <div className="flex flex-row justify-between md:flex-row md:justify-between p-5 md:items-center items-center relative w-full h-full pl-4 md:px-8 bg-gradient-to-t from-black/60 to-black/00 md:bg-gradient-to-t">
           <div className="text-left p-10">
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -114,7 +114,7 @@ export default function App() {
             <div className="flex flex-col">
               <p className="text-tiny text-white/60">Breathing App</p>
               <p className="text-tiny text-white/60">
-                Get a good night's sleep.
+                Get a good night&apos;s sleep.
               </p>
             </div>
           </div>
